3c2528e6d65df9a20845d3dbb8c2040dddc14bc5
Andrii Sotskyi <andrewsotskyi@gmail.com>
2021-05-19 20:15:00 -0500

    fix bug

--- app.js
+++ app.js
@@ -1,35 +1,28 @@
 const express = require("express");
 const app = express();
-const connectDB = require('./config/db');
-const cron =require('./cron.js')
-const path=require('path')
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+const connectDB = require("./config/db");
+const cron = require("./cron.js");
+const path = require("path");
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-app.use( express.json());
-const currencyValue = require('./routes/currencyValue')
- app.use("/api",currencyValue);
- 
+app.use(express.json());
+const currencyValue = require("./routes/currencyValue");
+app.use("/api", currencyValue);
+
 //  app.use("/",express.static(path.join(__dirname,"client","build")));
- require('dotenv').config({path:'./config/config.env'})
- connectDB()
+require("dotenv").config({ path: "./config/config.env" });
+connectDB();
 
-  // app.get("*",(req,res)=>{
-  //   res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+// app.get("*",(req,res)=>{
+//   res.sendFile(path.resolve(__dirname,"client","build","index.html"))
 
-
-
-  // })
+// })
 cron.job.start();
- app.listen(5000)
-
- 
-
- 
-
-
-
-
+app.listen(5000);
--- config/db.js
+++ config/db.js
@@ -1,21 +1,19 @@
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
 
-
-async   function connectDB(){
-    try{
-        await  mongoose.connect( process.env.MONGO_URI, {
-            useNewUrlParser:true,
-            useUnifiedTopology:true,
-            useCreateIndex:true})
-
-        
-        
-    }
-    catch(e){
-        console.log('server Error',e.message)
-        process.exit(1)
-    }
-
-
+async function connectDB() {
+  try {
+    await mongoose.connect(
+      (module.exports.mongoKey =
+        "mongodb+srv://andrew71241:71241@cluster0-qvxpn.mongodb.net/app?retryWrites=true&w=majority"),
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      }
+    );
+  } catch (e) {
+    console.log("server Error", e.message);
+    process.exit(1);
+  }
 }
-module.exports = connectDB
+module.exports = connectDB;
